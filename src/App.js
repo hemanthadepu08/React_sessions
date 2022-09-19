97de3f13426a2a3edaca66e5704fd00937a09e25
hemanthadepu08 <76736013+hemanthadepu08@users.noreply.github.com>
2022-09-19 15:01:47 +0530

    new changes in App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Profile } from "./Profile.js";
 export default function App() {
   return (
     <div className="App">
-      <h1 id="Company">Prodapt</h1>
+      <h1 id="Company">Prodapt one</h1>
       <Profile name={"Hemanth Adepu"} />
     </div>
   );
