a58ceb826c0a70400f1a6dcfd221fe9b4850403d
hemanthadepu08 <76736013+hemanthadepu08@users.noreply.github.com>
2022-09-19 12:50:55 +0530

    new changes in Styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Prodapt</h1>
+      <h1 id="Company">Prodapt</h1>
       <h2>Added new package react forms</h2>
     </div>
   );
