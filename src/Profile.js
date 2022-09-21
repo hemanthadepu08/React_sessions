37fd836892a1207f816547035ef88dd84d6d2ed7
hemanthadepu08 <76736013+hemanthadepu08@users.noreply.github.com>
2022-09-21 12:57:03 +0530

    changes

--- src/Profile.js
+++ src/Profile.js
@@ -2,14 +2,16 @@ import "./styles.css";
 function Profile(props) {
   return (
     <div className="App">
-      <img src={props.img} alt="Mark" />
-      <p>
-        <b>Name:</b>
-        {props.name}
-      </p>
-      <p>
-        <b>DOB:</b> {props.DOB}
-      </p>
+      <div id="profile">
+        <img src={props.img} alt="Mark" />
+        <p>
+          <b>Name:</b>
+          {props.name}
+        </p>
+        <p>
+          <b>DOB:</b> {props.DOB}
+        </p>
+      </div>
     </div>
   );
 }
